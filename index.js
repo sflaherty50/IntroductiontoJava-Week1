8b667bec7dcaad1b30b657a20a86898881e29a50
Sean <sean.flaherty2012@gmail.com>
2021-03-15 22:51:00 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var Itemprice =35.20;
+
+var Amountofmoneyinwallet =45.20;
+
+var Numberoffriends = 7;
+
+var Ageinyears = 27;
+
+var Firstname = "Jeremy";
+
+var Lastname = "Comic";
+
+var Middleinitial = "T";
+
+var Newamountofmoneyinwalletafterbuyingtheitem = Amountofmoneyinwallet - Itemprice;
+
+var	Numberoffriendsyouvemadeeachyearbasedonyouragevariableandyournumberoffriendsvariable = Numberoffriends / Ageinyears;
+
+var Fullnamebasedonfirstnamemiddleinitialandlastname = (Firstname +" " + Middleinitial +" "+ Lastname);
+
+
+console.log("My new balance in my wallet after my purchase is $" + Newamountofmoneyinwalletafterbuyingtheitem);
+
+console.log("The average number of friends I've made each year is " + Numberoffriendsyouvemadeeachyearbasedonyouragevariableandyournumberoffriendsvariable);
+
+console.log("My name is " + Fullnamebasedonfirstnamemiddleinitialandlastname);
